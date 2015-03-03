36902b248745f1e3e4451a83a9bf92259a0e6dfd
Timo Kasper <info@kasper-oswald.de>
2015-03-04 02:17:14 +0800

    Unique VID/PID, granted by Alexander Simonov, Thanks for supporting the project!

--- Firmware/Chameleon-Mini/LUFADescriptors.c
+++ Firmware/Chameleon-Mini/LUFADescriptors.c
@@ -54,8 +54,8 @@ const USB_Descriptor_Device_t PROGMEM DeviceDescriptor =
 
     .Endpoint0Size          = FIXED_CONTROL_ENDPOINT_SIZE,
 
-    .VendorID               = 0x03EB,
-    .ProductID              = 0x2044,
+ 		.VendorID 							= 0x16D0, // MCS Electronics (http://www.mcselec.com)
+ 		.ProductID 							= 0x04B2, // darksimpson's PID #2 granted to Chameleon-Mini
     .ReleaseNumber          = VERSION_BCD(00.01),
 
     .ManufacturerStrIndex   = 0x01,
