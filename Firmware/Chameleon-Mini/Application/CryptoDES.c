a90b89fa0fcf5d6462cc05b225cf87c0672af996
dev_zzo <gauri@tut.by>
2016-10-19 17:13:29 +0200

    desfire: implement authentication

---
+++ Firmware/Chameleon-Mini/Application/CryptoDES.c
@@ -0,0 +1,13 @@
+/*
+ * CryptoDES.c
+ *
+ *  Created on: 19.10.2016
+ *      Author: dev_zzo
+ */
+
+#include "CryptoDES.h"
+
+void CryptoEncrypt_3DES_KeyOption2_CBC(const uint8_t* Keys, void* Buffer, uint16_t Count)
+{
+	/* TODO :D */
+}
--- Firmware/Chameleon-Mini/Application/CryptoDES.h
+++ Firmware/Chameleon-Mini/Application/CryptoDES.h
@@ -8,6 +8,11 @@
 #ifndef CRYPTODES_H_
 #define CRYPTODES_H_
 
+#include <stdint.h>
+
 #define CRYPTO_DES_KEY_SIZE 8
+
+/** Performs the Triple DES sequence in the CBC mode on the buffer */
+void CryptoEncrypt_3DES_KeyOption2_CBC(const uint8_t* Keys, void* Buffer, uint16_t Count);
 
 #endif /* CRYPTODES_H_ */
