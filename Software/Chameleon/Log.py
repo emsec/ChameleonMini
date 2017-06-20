63c8fb481284dce6460f451f9a57a364951b3fd9
Georg <georg@kasper-oswald.de>
2017-06-20 18:33:35 +0200

    chamlog python update

--- Software/Chameleon/Log.py
+++ Software/Chameleon/Log.py
@@ -17,13 +17,13 @@ eventTypes = {
     0x10: { 'name': 'GENERIC',        'decoder': textDecoder },
     0x11: { 'name': 'CONFIG SET',     'decoder': textDecoder },
     0x12: { 'name': 'SETTING SET',    'decoder': textDecoder },
-    0x13: { 'name': 'UID SET',        'decoder': textDecoder },
+    0x13: { 'name': 'UID SET',        'decoder': binaryDecoder },
     0x20: { 'name': 'RESET APP',      'decoder': noDecoder },
 
     0x40: { 'name': 'CODEC RX',       'decoder': binaryDecoder },
     0x41: { 'name': 'CODEC TX',       'decoder': binaryDecoder },
-    0x42: { 'name': 'CODEC RX W/ PARITY', 'decoder': binaryDecoder },
-    0x43: { 'name': 'CODEC TX W/ PARITY', 'decoder': binaryDecoder },
+    0x42: { 'name': 'CODEC RX W/PARITY', 'decoder': binaryDecoder },
+    0x43: { 'name': 'CODEC TX W/PARITY', 'decoder': binaryDecoder },
 
     0x80: { 'name': 'APP READ',       'decoder': binaryDecoder },
     0x81: { 'name': 'APP WRITE',      'decoder': binaryDecoder },
@@ -34,19 +34,20 @@ eventTypes = {
     
     0x90: { 'name': 'APP AUTH',       'decoder': binaryDecoder },
     0x91: { 'name': 'APP HALT',       'decoder': binaryDecoder },
-    0x92: { 'name': 'APP UNKNOWN',       'decoder': binaryDecoder },
+    0x92: { 'name': 'APP UNKNOWN',    'decoder': binaryDecoder },
     
-    0xA0: { 'name': 'APP AUTHING',    'decoder': binaryDecoder },
+    0xA0: { 'name': 'APP AUTHING' ,    'decoder': binaryDecoder },
     0xA1: { 'name': 'APP AUTHED',      'decoder': binaryDecoder },
 
     0xC0: { 'name': 'APP AUTH FAILED', 'decoder': binaryDecoder },
     0xC1: { 'name': 'APP CSUM FAILED', 'decoder': binaryDecoder },
     0xC2: { 'name': 'APP NOT AUTHED',  'decoder': binaryDecoder },
     
-    0xFF: { 'name': 'BOOT',            'decoder': noDecoder },
+    0xFF: { 'name': 'BOOT',            'decoder': binaryDecoder },
 }
 
 TIMESTAMP_MAX = 65536
+eventTypes = { i : ({'name': 'UNKNOWN', 'decoder': binaryDecoder} if i not in eventTypes.keys() else eventTypes[i]) for i in range(256) }
 
 def parseBinary(binaryStream):
     log = []
