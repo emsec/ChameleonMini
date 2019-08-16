0fdc1b43ceb734490d206caee1d229170a617c5c
Lava Git <lavanoid@gmail.com>
2019-08-16 14:27:48 +0100

    Add Mifare Mini support

--- Firmware/Chameleon-Mini/Application/MifareClassic.h
+++ Firmware/Chameleon-Mini/Application/MifareClassic.h
@@ -11,10 +11,12 @@
 #include "Application.h"
 #include "ISO14443-3A.h"
 
-#define MIFARE_CLASSIC_UID_SIZE     ISO14443A_UID_SIZE_SINGLE
-#define MIFARE_CLASSIC_1K_MEM_SIZE  1024
-#define MIFARE_CLASSIC_4K_MEM_SIZE  4096
+#define MIFARE_CLASSIC_UID_SIZE       ISO14443A_UID_SIZE_SINGLE
+#define MIFARE_CLASSIC_MINI_MEM_SIZE  320
+#define MIFARE_CLASSIC_1K_MEM_SIZE    1024
+#define MIFARE_CLASSIC_4K_MEM_SIZE    4096
 
+void MifareClassicAppInitMini4B(void);
 void MifareClassicAppInit1K(void);
 void MifareClassicAppInit4K(void);
 void MifareClassicAppInit1K7B(void);
