5550461dd45bbde44149cc7b016f63543edd4f75
Giovanni Cammisa <g.cammisa@studenti.unibs.it>
2020-06-13 18:48:06 +0200

    Fixed a typo in NTAG215.h
    Fixed the typo pointed out by @HexRabbit

--- Firmware/Chameleon-Mini/Application/NTAG215.h
+++ Firmware/Chameleon-Mini/Application/NTAG215.h
@@ -14,7 +14,7 @@
 #define NTAG215_UID_SIZE ISO14443A_UID_SIZE_DOUBLE //7 bytes UID
 #define NTAG215_PAGE_SIZE 4 //bytes per page
 #define NTAG215_PAGES 135 //135 pages total, from 0 to 134
-#define NTAG215_MEM_SIZE ( NTAG215_PAGE_SIZE * NTAG215_PAGE_SIZE )
+#define NTAG215_MEM_SIZE ( NTAG215_PAGE_SIZE * NTAG215_PAGES )
 
 void NTAG215AppInit(void);
 void NTAG215AppReset(void);
